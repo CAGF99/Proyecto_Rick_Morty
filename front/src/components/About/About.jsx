af7085f7be4835a734d7124dd0788ea32f283302
CAGF99 <cesargonzalesfarfan@gmail.com>
2023-03-02 23:12:10 -0500

    Probando cambios

--- front/src/components/About/About.jsx
+++ front/src/components/About/About.jsx
@@ -4,8 +4,6 @@ const About = () =>{
     return(
         <div>
             <h2>César Gonzales Farfan</h2>
-            <h3>Cosas de mi:</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga repellat sit, ullam vero non reiciendis. Alias, perferendis, suscipit officia reiciendis explicabo commodi numquam voluptatibus magni quas officiis culpa assumenda perspiciatis.</p>
         </div>
     )
 
